6e6dd06f7a1915af8c40238a00c4a766a6c4a378
HitoDZ <hitodz97@gmail.com>
2017-05-27 23:31:22 +0300

    Beckup
    almost finish ^_^

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-function myFunction(form) {
+function main_part(form) {
 
 
     var m = form.n.value;
@@ -10,85 +10,118 @@ function myFunction(form) {
         var mas_b = Array(n);
         var k = 0;
 
-        for (var i = 0; i < n; i++) {
+        var ab = parseInt(n) + 1;
 
+        document.getElementById("demo").innerHTML = '';
 
-
-            if (i != 0) {
-                document.getElementById("demo").innerHTML += '</br>';
-            } else {
-                document.getElementById("demo").innerHTML = '';
-            }
+        for (var i = 0; i < ab; i++) {
 
             for (var j = 0; j < m; j++) {
-                var gen = 'game_' + k;
-                r = parseInt((Math.random() * 42) - 21);
-                // Player B max
-                if (i != 0) {
-                    mas_b[j].val = Math.max(mas_b[j].val, r);
-                    if (mas_b[j].val == r) {
-                        mas_b[j].id = gen;
-                    }
-                } else {
-                    mas_b[j] = {
-                        id: gen,
-                        val: r
-                    };
-                }
-                // Player A min
-                if (j != 0) {
-                    mas_a[i].val = Math.min(mas_a[i].val, r);
-                    if (mas_a[i].val == r) {
-                        mas_a[i].id = gen;
-                    }
-                } else {
-                    mas_a[i] = {
-                        id: gen,
-                        val: r
-                    };
-                }
 
-                document.getElementById("demo").innerHTML += '<div class="window" ' + 'id="' + gen + '"">' + r + '</div>';
+                if (i != n) {
+                    var gen = 'game_' + k;
+                    r = parseInt((Math.random() * 42) - 21);
 
-                k++;
+                    // Player B max
+                    if (i != 0) {   
+                        mas_b[j].val = Math.max(mas_b[j].val, r);
+                        if (mas_b[j].val == r) {
+                            mas_b[j].id = gen;
+                        }
+
+                    } else {
+                        mas_b[j] = {
+                            id: gen,
+                            val: r
+                        };
+                    }
+                    // Player A min
+                    if (j != 0) {
+                        mas_a[i].val = Math.min(mas_a[i].val, r);
+                        if (mas_a[i].val == r) {
+                            mas_a[i].id = gen;
+                        }
+                    } else {
+                        mas_a[i] = {
+                            id: gen,
+                            val: r
+                        };
+                    }
+
+                    document.getElementById("demo").innerHTML += '<div class="window" ' + 'id="' + gen + '"">' + r + '</div>';
+
+                    k++;
+                } else { document.getElementById("demo").innerHTML += '<div class="window_ans2" ' + 'id="' + gen + '"">' + mas_b[j].val + '</div>'; }
             }
+            if (i != n ) {
+                document.getElementById("demo").innerHTML += '<div class="window_ans1" ' + 'id="' + gen + '"">' + mas_a[i].val + '</div>';
+            }
+            document.getElementById("demo").innerHTML += '</br>';
         }
+
         // Results for Players A and B
         var mas_a_max = {
             id: mas_a[0].id,
+            id2: null,
             val: mas_a[0].val
         };
         var mas_b_min = {
             id: mas_b[0].id,
+            id2: null,
             val: mas_b[0].val
         };
 
+        var check = 0;
+
         for (var i = 1; i < n; i++) {
+
+
+            if (mas_a_max.val == mas_a[i].val) {
+                mas_a_max.id2 = mas_a_max.id;
+                check = mas_a_max.val;
+            }
 
             mas_a_max.val = Math.max(mas_a_max.val, mas_a[i].val);
 
             if (mas_a_max.val == mas_a[i].val) {
                 mas_a_max.id = mas_a[i].id;
+
+                if (check != mas_a_max.val) { mas_a_max.id2 = null; }
+
             }
         }
 
         for (var i = 1; i < m; i++) {
 
+            if (mas_b_min.val == mas_b[i].val) {
+                mas_b_min.id2 = mas_b_min.id;
+                check = mas_b_min.val;
+            }
             mas_b_min.val = Math.min(mas_b_min.val, mas_b[i].val);
 
             if (mas_b_min.val == mas_b[i].val) {
                 mas_b_min.id = mas_b[i].id;
+
+                if (check != mas_b_min.val) { mas_b_min.id2 = null; }
             }
         }
 
-        //document.getElementById("demo").innerHTML += '<div class="window">' + mas_a_max.val + '</div>';
-        //document.getElementById("demo").innerHTML += '<div class="window">' + mas_b_min.val + '</div>';
+
 
         if (mas_a_max.id != mas_b_min.id) {
+
             document.getElementById(mas_a_max.id).className = "wind_color1";
+            if (mas_a_max.id2 != null) { document.getElementById(mas_a_max.id2).className = "wind_color1"; }
+
+            if (mas_b_min.id2 != null) { document.getElementById(mas_b_min.id2).className = "wind_color2"; }
             document.getElementById(mas_b_min.id).className = "wind_color2";
+
+            document.getElementById("demo").innerHTML += '<br/><div class="ans">' + 'Ответ A: ' + mas_a_max.val + '</div>';
+            document.getElementById("demo").innerHTML += '<div class="ans">' + 'Ответ B: ' + mas_b_min.val + '</div>';
+            
         } else {
             document.getElementById(mas_a_max.id).className = "wind_color3";
+            document.getElementById("demo").innerHTML += '<br/><br/><div class="ans">' + 'Ответ: ' + mas_a_max.val + '</div>';
         }
 
     } else {
@@ -98,15 +131,17 @@ function myFunction(form) {
 }
 
 // --------------\
+
+
 function new_post() {
-    
+
     var div = document.createElement('div');
-    div.className = "modal_box";
-    div.innerHTML = "<h3>Неверные данные</h3>" +
-    "<input type='button' value='Ok' class='okey' onclick=remove_modalBox()>" +
-    "</form>";
+    div.className = "all_my";
+    div.innerHTML = "<div class='modal_box'><p style='overflow-x:hidden; font-size: 25px; font-family: Impact;' > Неверные данные</p>" + "<p style='overflow-x:hidden;font-size: 17px;font-family: Impact;'>Попробуйте &nbsp; 2&le; m &le; 5 &nbsp; и  &nbsp; 2&le; n &le; 6</p>"  + '<br/>' +
+    "<input type='button' value='Ok' class='okey' onclick=remove_modalBox()></div>";
+
     document.body.appendChild(div);
-    
+
 }
 function remove_modalBox() {
     document.body.removeChild(document.body.lastChild)
