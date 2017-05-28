b97fe8462a17cacd1387258fcda97694ed65a1da
HitoDZ <hitodz97@gmail.com>
2017-05-28 13:33:00 +0300

    Revert "Final_version"
    This reverts commit f9a19ec01040f79154f66e3d3419827803072833.

--- static/script.js
+++ static/script.js
@@ -11,22 +11,10 @@ function main_part(form) {
         var k = 0;
 
         var ab = parseInt(n) + 1;
-        var am = parseInt(m) + 2;
+
         document.getElementById("demo").innerHTML = '';
 
-        for (var z = 0 ; z < am ; z++) {
-            if (z != 0 && z != am-1) { document.getElementById("demo").innerHTML += '<div class="window_b" >' + 'B' + z + '</div>'; }
-            else if (z == 0) { document.getElementById("demo").innerHTML += '<div class="window_b" ></div>'; }
-            else { document.getElementById("demo").innerHTML += '<div class="window_b" >' + 'min_A'  + '</div>'; }
-        }
-        document.getElementById("demo").innerHTML += '</br>';
-
         for (var i = 0; i < ab; i++) {
-
-            need = parseInt(i) + 1;
-            if (need != ab) {
-                document.getElementById("demo").innerHTML += '<div class="window_b" >' + 'A' + need + '</div>';
-            } else { document.getElementById("demo").innerHTML += '<div class="window_b" >' + 'max_B' + '</div>'; }
 
             for (var j = 0; j < m; j++) {
 
@@ -35,7 +23,7 @@ function main_part(form) {
                     r = parseInt((Math.random() * 42) - 21);
 
                     // Player B max
-                    if (i != 0) {
+                    if (i != 0) {   
                         mas_b[j].val = Math.max(mas_b[j].val, r);
                         if (mas_b[j].val == r) {
                             mas_b[j].id = gen;
@@ -65,12 +53,10 @@ function main_part(form) {
                     k++;
                 } else { document.getElementById("demo").innerHTML += '<div class="window_ans2" ' + 'id="' + gen + '"">' + mas_b[j].val + '</div>'; }
             }
-            if (i != n) {
+            if (i != n ) {
                 document.getElementById("demo").innerHTML += '<div class="window_ans1" ' + 'id="' + gen + '"">' + mas_a[i].val + '</div>';
             }
             document.getElementById("demo").innerHTML += '</br>';
-            
-            
         }
 
         // Results for Players A and B
@@ -130,12 +116,12 @@ function main_part(form) {
             if (mas_b_min.id2 != null) { document.getElementById(mas_b_min.id2).className = "wind_color2"; }
             document.getElementById(mas_b_min.id).className = "wind_color2";
 
-            document.getElementById("demo").innerHTML += '<br/><div class="ans">' + '<p style="overflow-x:hidden;font-size: 20px;font-family: Impact;">Ответ A : &nbsp; ' + mas_a_max.val + '</p></div>';
-            document.getElementById("demo").innerHTML += '<div class="ans">' + '<p style="overflow-x:hidden;font-size: 20px;font-family: Impact;">Ответ B:  &nbsp;  ' + mas_b_min.val + '</p></div>';
+            document.getElementById("demo").innerHTML += '<br/><div class="ans">' + 'Ответ A: ' + mas_a_max.val + '</div>';
+            document.getElementById("demo").innerHTML += '<div class="ans">' + 'Ответ B: ' + mas_b_min.val + '</div>';
             
         } else {
             document.getElementById(mas_a_max.id).className = "wind_color3";
-            document.getElementById("demo").innerHTML += '<br/><br/><div class="ans" >' + '<p style="overflow-x:hidden;font-size: 20px;font-family: Impact;">Ответ: ' + mas_a_max.val + '</p></div>';
+            document.getElementById("demo").innerHTML += '<br/><br/><div class="ans">' + 'Ответ: ' + mas_a_max.val + '</div>';
         }
 
     } else {
@@ -151,7 +137,7 @@ function new_post() {
 
     var div = document.createElement('div');
     div.className = "all_my";
-    div.innerHTML = "<div class='modal_box'><img src='static/Att.png   ' alt='альтернативный текст'> <p style='overflow-x:hidden; font-size: 25px; font-family: Impact;' > Неверные данные</p>" + "<p style='overflow-x:hidden;font-size: 17px;font-family: Impact;'>Попробуйте &nbsp; 2&le; m &le; 5 &nbsp; и  &nbsp; 2&le; n &le; 6</p>"  + '<br/>' +
+    div.innerHTML = "<div class='modal_box'><p style='overflow-x:hidden; font-size: 25px; font-family: Impact;' > Неверные данные</p>" + "<p style='overflow-x:hidden;font-size: 17px;font-family: Impact;'>Попробуйте &nbsp; 2&le; m &le; 5 &nbsp; и  &nbsp; 2&le; n &le; 6</p>"  + '<br/>' +
     "<input type='button' value='Ok' class='okey' onclick=remove_modalBox()></div>";
 
     document.body.appendChild(div);
